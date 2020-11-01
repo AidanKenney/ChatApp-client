f2ff12df76ddeb4846aeda522661b7ac741948e7
AidanKenney <kenneyaidan@gmail.com>
2020-11-01 15:28:43 -0500

    Changed apiConfig URL for production

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://chatapp-api-django.herokuapp.com',
   development: 'http://localhost:8000'
 }
 
